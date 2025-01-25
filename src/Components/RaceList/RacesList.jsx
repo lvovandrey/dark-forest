e4458eb37f1c8a69774752f4d1a59ee80369c904
aa.lvov@i-teco.ru <aa.lvov@i-teco.ru>
2025-01-25 18:57:54 +0300

    еще чуть вынес формирование элементов на основе массива данных

--- src/Components/RaceList/RacesList.jsx
+++ src/Components/RaceList/RacesList.jsx
@@ -2,14 +2,14 @@ import RaceLink from './Common/RaceLink/RaceLink';
 import css from './RacesList.module.css'
 
 const RacesList = (props) => {
+
+    let racesLinksElements = props.races.map(race => (<RaceLink race={race} />));
+
     return (
         <div className={css.racesList}>
             <h1>RacesList</h1>
             <div className={css.list}>
-                <RaceLink race={props.races[0]} />
-                <RaceLink race={props.races[1]} />
-                <RaceLink race={props.races[2]} />
-                <RaceLink race={props.races[3]} />
+                { racesLinksElements }
             </div>
         </div>
     )
