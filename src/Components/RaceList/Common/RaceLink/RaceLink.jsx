30993ada04313a0a99beec8d14a73e18590d8f93
aa.lvov@i-teco.ru <aa.lvov@i-teco.ru>
2025-01-25 18:36:22 +0300

    Немного отделил данные от UI

--- src/Components/RaceList/Common/RaceLink/RaceLink.jsx
+++ src/Components/RaceList/Common/RaceLink/RaceLink.jsx
@@ -3,10 +3,10 @@ import PlayerCard from '../../../PlayerCard/PlayerCard';
 import css from './RaceLink.module.css';
 
 const RaceLink = (props) => {
-    let path = `/race/${props.raceid}`;
+    let path = `/race/${props.race.id}`;
     return (
         <NavLink to={path}>
-            <PlayerCard race={props.race} streight={props.streight} health={props.health}/>
+            <PlayerCard race={props.race.name} streight={props.race.streight} health={props.race.health} />
         </NavLink>
     )
 }
--- src/Components/RaceList/RacesList.jsx
+++ src/Components/RaceList/RacesList.jsx
@@ -1,14 +1,15 @@
 import RaceLink from './Common/RaceLink/RaceLink';
 import css from './RacesList.module.css'
 
-const RacesList = () => {
+const RacesList = (props) => {
     return (
         <div className={css.racesList}>
             <h1>RacesList</h1>
             <div className={css.list}>
-                <RaceLink raceid='1' race='Силикоиды' streight='21' health='25' />
-                <RaceLink raceid='2' race='Терраны' streight='10' health='10' />
-                <RaceLink raceid='3' race='Псилоны' streight='41' health='3' />
+                <RaceLink race={props.races[0]} />
+                <RaceLink race={props.races[1]} />
+                <RaceLink race={props.races[2]} />
+                <RaceLink race={props.races[3]} />
             </div>
         </div>
     )
