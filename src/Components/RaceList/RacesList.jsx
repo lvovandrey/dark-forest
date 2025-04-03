c8a8fafe5cbd7c31d00c949d5430abf31fe42327
aa.lvov@i-teco.ru <aa.lvov@i-teco.ru>
2025-04-03 22:02:16 +0300

    useEffect - в одной компоненте

--- src/Components/RaceList/RacesList.jsx
+++ src/Components/RaceList/RacesList.jsx
@@ -1,43 +1,74 @@
 import ParameterEditor from '../Common/ParameterEditor/ParameterEditor';
 import RaceLink from './Common/RaceLink/RaceLink';
 import css from './RacesList.module.css'
-import React from 'react';
+import React, { useEffect } from 'react';
 
-class RacesList extends React.Component {
+const RacesList = (props) => {
 
-    loadRaces = () => {
-        if (this.props.races.length > 0) {
-            this.props.loadFullRacesListTC()
+    let loadRaces = () => {
+        if (props.races.length > 0) {
+            props.loadFullRacesListTC()
         }
     }
 
-    componentDidMount() {
-        this.loadRaces()
-    }
+    useEffect(() => loadRaces())
 
-    componentDidUpdate() {
-        this.loadRaces()
-    }
+    const racesLinksElements = props.races?.map(race => (<RaceLink key={race.id} race={race} />));
+    const addNewRace = () => props.addNewRace()
+    const setValue = (parameter, value) => props.onChangePreCreatedRaceName(value)
 
-    render () {
-        const racesLinksElements = this.props.races?.map(race => (<RaceLink key={race.id} race={race} />));
-        const addNewRace = () => this.props.addNewRace()
-        const setValue = (parameter, value) => this.props.onChangePreCreatedRaceName(value)
 
-        return (
-            <div className={css.racesList}>
-                <button onClick={()=>this.loadRaces()}>Reload races</button>
-                <h1>RacesList</h1>
-                <div className={css.editFieldContainer}>
-                    <ParameterEditor title='Название' parameter='name' setValue={setValue} value={this.props.preCreatedRaceName} />
-                    <button onClick={addNewRace}> Create </button>
-                </div>
-                <div className={css.list}>
-                    {racesLinksElements}
-                </div>
+    return (
+        <div className={css.racesList}>
+            <button onClick={() => loadRaces()}>Reload races</button>
+            <h1>RacesList</h1>
+            <div className={css.editFieldContainer}>
+                <ParameterEditor title='Название' parameter='name' setValue={setValue} value={props.preCreatedRaceName} />
+                <button onClick={addNewRace}> Create </button>
             </div>
-        )
-    }
+            <div className={css.list}>
+                {racesLinksElements}
+            </div>
+        </div>
+    )
 }
+
+
+// class RacesList extends React.Component {
+
+//     loadRaces = () => {
+//         if (this.props.races.length > 0) {
+//             this.props.loadFullRacesListTC()
+//         }
+//     }
+
+//     componentDidMount() {
+//         this.loadRaces()
+//     }
+
+//     componentDidUpdate() {
+//         this.loadRaces()
+//     }
+
+//     render () {
+//         const racesLinksElements = this.props.races?.map(race => (<RaceLink key={race.id} race={race} />));
+//         const addNewRace = () => this.props.addNewRace()
+//         const setValue = (parameter, value) => this.props.onChangePreCreatedRaceName(value)
+
+//         return (
+//             <div className={css.racesList}>
+//                 <button onClick={()=>this.loadRaces()}>Reload races</button>
+//                 <h1>RacesList</h1>
+//                 <div className={css.editFieldContainer}>
+//                     <ParameterEditor title='Название' parameter='name' setValue={setValue} value={this.props.preCreatedRaceName} />
+//                     <button onClick={addNewRace}> Create </button>
+//                 </div>
+//                 <div className={css.list}>
+//                     {racesLinksElements}
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default RacesList;
